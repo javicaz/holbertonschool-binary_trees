3473bb0a52a08d91b7ca7e9b3c474223485ad4b0
Oscar Guerra <oogl29@gmail.com>
2023-01-12 22:43:43 -0500

    Create 1-binary_tree_insert_left.c and update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -30,7 +30,9 @@ typedef struct binary_tree_s heap_t;
 /* function print */
 void binary_tree_print(const binary_tree_t *tree);
 
-/*TASK 0*/
+/*Task 0*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+/*Task 1*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
