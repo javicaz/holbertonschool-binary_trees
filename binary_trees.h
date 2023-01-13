c8675a05738dff8025bb9ba839a0f364b5a20293
Oscar Guerra <oogl29@gmail.com>
2023-01-12 23:34:26 -0500

    Create 5-binary_tree_is_root.c and update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -40,5 +40,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 /*Task 4*/
 int binary_tree_is_leaf(const binary_tree_t *node);
+/*Task 5*/
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
